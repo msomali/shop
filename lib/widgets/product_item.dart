5a8d007bf7a54ca5df1b1c29c49f59f000833519
msomali <waleed92@live.com>
2021-12-21 22:17:16 +0300

    snackbar added

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -74,6 +74,19 @@ class ProductItem extends StatelessWidget {
                 product.title,
                 product.price,
               );
+              Scaffold.of(context).hideCurrentSnackBar();
+              Scaffold.of(context).showSnackBar(SnackBar(
+                content: Text(
+                  'Added item to cart!',
+                ),
+                duration: Duration(seconds: 2),
+                action: SnackBarAction(
+                  label: 'UNDO',
+                  onPressed: () {
+                    cart.removeSingleItem(product.id);
+                  },
+                ),
+              ));
             },
           ),
         ),
